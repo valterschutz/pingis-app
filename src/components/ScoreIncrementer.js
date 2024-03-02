b1385c1e71c66ed45804b208c8de96b74a6e31d2
Valter Schutz <valterschutz@proton.me>
2024-03-02 21:55:21 +0100

    pingpongred navbar

--- src/components/ScoreIncrementer.js
+++ src/components/ScoreIncrementer.js
@@ -3,7 +3,7 @@ import AnimatedButton from "./AnimatedButton"
 export default function ScoreIncrementer({ score, setScore }) {
   return <div className="flex flex-row justify-center items-center gap-2">
     <AnimatedButton classes="shadow-md text-4xl font-bold h-20 w-20 bg-pingpongred text-white border-opacity-95 border-white rounded-full pb-1.5 hover:bg-scarlet" onClick={() => { setScore(score - 1) }}>-</AnimatedButton>
-    <input className="shadow-md text-4xl h-12 w-40 rounded-lg text-center font-kanit" type="number" value={score} onChange={e => {
+    <input className="shadow-md text-4xl h-12 w-40 rounded-lg text-center font-kode font-bold" type="number" value={score} onChange={e => {
       setScore(parseInt(e.target.value))
     }} />
     <AnimatedButton classes="shadow-md text-4xl font-bold h-20 w-20 bg-pingpongred text-white border-opacity-95 border-white rounded-full pb-1.5 hover:bg-scarlet" onClick={() => { setScore(score + 1) }}>+</AnimatedButton>
